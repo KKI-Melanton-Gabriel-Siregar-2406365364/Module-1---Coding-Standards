323ed0a0080c6d0e92c070af2c7d4d2e37abeb61
Twentism <melantongabriel@gmail.com>
2026-03-10 10:44:10 +0700

    [REFACTOR] Apply OrderStatus enum to Order model tests.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -49,8 +49,8 @@ class OrderTest {
 
     @Test
     void testCreateOrderSuccessStatus() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat", "SUCCESS");
-        assertEquals("SUCCESS", order.getStatus());
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat", id.ac.ui.cs.advprog.eshop.enums.OrderStatus.SUCCESS.getValue());
+        assertEquals(id.ac.ui.cs.advprog.eshop.enums.OrderStatus.SUCCESS.getValue(), order.getStatus());
     }
 
     @Test
